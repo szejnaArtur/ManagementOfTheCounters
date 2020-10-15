cf8966f00baa67b6b70864c9a58e53ac48e019bb
Artur <szejnaartur@gmail.com>
2020-10-15 14:20:51 +0200

    create spring boot project

---
+++ src/main/java/pl/szejnaArtur/ManagementOfTheCounters/ManagementOfTheCountersApplication.java
@@ -0,0 +1,13 @@
+package pl.szejnaArtur.ManagementOfTheCounters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementOfTheCountersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagementOfTheCountersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/szejnaArtur/ManagementOfTheCounters/ManagementOfTheCountersApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.szejnaArtur.ManagementOfTheCounters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementOfTheCountersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
